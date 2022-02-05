0dedd17dd37c2621a945def6aed87eb4f58de625
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-05 13:33:36 -0800

    commit tree 3d143f71d96c0ddfe6aac56b652ff9ea239a60dd

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 
-// Write your code here
-let multiply;
-    let num1 = 2
-    let num2 = 31
-    multiply = num1*num2
+// // Write your code here
+// let multiply;
+//     let num1 = 2
+//     let num2 = 31
+//     multiply = num1*num2
 
-    let mod;
-    let num3 = 12
-    let num4 = 3
-    mod = num3/num4
+//     let mod;
+//     let num3 = 12
+//     let num4 = 3
+//     mod = num3/num4
 
-    let max;
-    max = Math.max (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+//     let max;
+//     max = Math.max (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 
-    let random;
-    random = Math.ceil(Math.random())
+//     let random;
+//     random = Math.ceil(Math.random())
+
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor (Math.random()*10) +1
+
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+let max= Math.max(1,4,20)
